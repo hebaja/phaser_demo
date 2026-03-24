cfd6bb977cbdec67a0e5d3c338210ce221ca6a64
hebatist <hebaja@yahoo.com.br>
2026-03-24 18:56:30 -0300

    enemy added

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,15 +1,18 @@
 import { Scene } from 'phaser';
 import { Player } from '../objects/Player';
+import { Enemy } from '../objects/Enemy';
 import { Gold } from '../objects/Gold';
 
 export class Game extends Scene
 {
     keys: any
     player: Player
+	enemy: Enemy
     gold_a: any
     gold_b: any
     golds: Gold[] = []
     isAttacking: boolean = false
+	attackHitbox: any 
 
     constructor ()
     {
@@ -66,6 +69,18 @@ export class Game extends Scene
            frameHeight: 192,
            frameWidth: 192
         })
+        this.load.spritesheet('enemy', 'enemy_run.png', {
+           frameHeight: 150,
+           frameWidth: 150
+        })
+        this.load.spritesheet('enemy_death', 'enemy_death.png', {
+           frameHeight: 150,
+           frameWidth: 150
+        })
+        this.load.spritesheet('enemy_take_hit', 'enemy_take_hit.png', {
+           frameHeight: 150,
+           frameWidth: 150
+        })
     }
 
     create ()
@@ -100,6 +115,24 @@ export class Game extends Scene
             frameRate: 8,
             repeat: 2
         })
+        this.anims.create({
+            key: 'enemy_run',
+            frames: this.anims.generateFrameNumbers('enemy', {frames: [0, 1, 2, 3, 4, 5, 6, 7]}),
+            frameRate: 8,
+            repeat: 2
+        })
+        this.anims.create({
+            key: 'enemy_death',
+            frames: this.anims.generateFrameNumbers('enemy_death', {frames: [0, 1, 2, 3]}),
+            frameRate: 8,
+            repeat: 2
+        })
+        this.anims.create({
+            key: 'enemy_take_hit',
+            frames: this.anims.generateFrameNumbers('enemy_death', {frames: [0, 1, 2, 3]}),
+            frameRate: 8,
+            repeat: 2
+        })
 
         const groundPlatform = this.createPlatform(14, 1010, 32 * 3, 0, 1, 5)
 
@@ -107,19 +140,33 @@ export class Game extends Scene
         const floatingPlatformBottom = this.createPlatform(564, 450 + 32, 8, 126, 127, 131)
 
         this.player = new Player(this, 0, 900, 'player_idle')
+		this.enemy = new Enemy(this, 900, 900, 'enemy')
+
+		this.attackHitbox = this.physics.add.sprite(this.player.x, this.player.y, "")
+		this.attackHitbox.body.setSize(40, 110)
+		this.attackHitbox.setVisible(false)
+		this.attackHitbox.body.enable = false
+
+		this.attackHitbox.body.setAllowGravity(false)
 
         this.golds.push(new Gold(this, 512, 900, 'gold'))
         this.golds.push(new Gold(this, 712, 900, 'gold'))
 
         this.player.body?.setSize(84, 70)
         this.golds[0].body?.setSize(55, 35)
+		this.enemy.body?.setSize(20, 50)
         this.physics.add.collider(this.player, groundPlatform)
+        this.physics.add.collider(this.enemy, groundPlatform)
 
         this.physics.add.collider(this.golds[0], groundPlatform)
         this.physics.add.collider(this.golds[1], groundPlatform)
         
         this.physics.add.collider(this.player, floatingPlatformUpper)
         this.physics.add.collider(this.player, floatingPlatformBottom)
+        
+		// this.physics.add.collider(this.player, this.enemy)
+		// this.physics.add.overlap(this.player, this.enemy, this.hitEnemy, undefined, this)
+		this.physics.add.overlap(this.attackHitbox, this.enemy, this.hitEnemy, undefined, this)
 
         this.createFloatingPlatform(256, 705, 16)
         this.createFloatingPlatform(564, 450, 8)
@@ -147,14 +194,31 @@ export class Game extends Scene
         this.player.on('animationcomplete-attack', () => {
             this.isAttacking = false;
         });
-
-
     }
+
+	hitEnemy(_player: any, _enemy: any)
+	{
+		if (this.isAttacking) {
+			_enemy.disableBody(true, true)
+		}
+	}
 
     update ()
     {
+		const isAnimAttack = this.player.anims.currentAnim?.key === 'player_attack' && this.player.anims.isPlaying
+
+
+
         if (this.keys.J.isDown && !this.isAttacking) {
             this.isAttacking = true;
+			// this.player.body?.setSize(124, 70)
+			this.time.delayedCall(250, () => {
+				this.attackHitbox.body.enable = true;
+			})
+			if (this.player.flipX)
+				this.attackHitbox.setPosition(this.player.x - 70, this.player.y)
+			else
+				this.attackHitbox.setPosition(this.player.x + 70, this.player.y)
             this.player.play('attack');
         }
 
@@ -173,6 +237,10 @@ export class Game extends Scene
             else {
                 this.player.play('idle', true)
             }
+			// this.player.body?.setSize(84, 70)
+			//
+			if (!isAnimAttack)
+				this.attackHitbox.body.enable = false;
 
         }
 
@@ -180,6 +248,9 @@ export class Game extends Scene
         if ((this.keys.up.isDown || this.keys.W.isDown) && this.player.body?.touching.down) {
             this.player.setVelocityY(-3000)
         }
+		this.enemy.setFlipX(true)
+		this.enemy.setVelocityX(-200)
+		this.enemy.play('enemy_run', true)
     }
 
     collectGold(_player: any, _gold: any)
